607536948f179c424204e7b23cef0f9b64f3592c
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2024-01-29 13:31:17 +0100

    Control the LED

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::{thread, time::Duration};
+
+use esp_idf_svc::hal::peripherals::Peripherals;
+use ws2812_esp32_rmt_driver::{
+    driver::color::{LedPixelColor, LedPixelColorGrb24},
+    Ws2812Esp32RmtDriver,
+};
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +15,23 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().expect("Unable to access device peripherals");
+    let led_pin = peripherals.pins.gpio8;
+    let rmt_channel = peripherals.rmt.channel0;
+    let mut neopixel =
+        Ws2812Esp32RmtDriver::new(rmt_channel, led_pin).expect("Unable to talk to ws2812");
+    let color_1 = LedPixelColorGrb24::new_with_rgb(255, 255, 0);
+    let color_2 = LedPixelColorGrb24::new_with_rgb(255, 0, 255);
+
+    loop {
+        neopixel
+            .write_blocking(color_1.as_ref().iter().cloned())
+            .expect("Error writing to neopixel");
+        thread::sleep(Duration::from_millis(500));
+        neopixel
+            .write_blocking(color_2.as_ref().iter().cloned())
+            .expect("Error writing to neopixel");
+        thread::sleep(Duration::from_millis(500));
+    }
 }
